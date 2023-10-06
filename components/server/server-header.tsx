613dd4e689c223d8caebd952b5d558ea9934dcc7
anh.nt <anh.nt@vn-cubesystem.com>
2023-10-06 10:53:10 +0700

    test commit

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -2,6 +2,7 @@
 
 import { ServerWithMembersWithProfiles } from "@/type";
 import { MemberRole } from "@prisma/client";
+import { ChevronDown } from "lucide-react";
 
 import {
   DropdownMenu,
@@ -19,8 +20,11 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="">
-
+      <DropdownMenuTrigger className="focus:outline-none" asChild>
+        <button className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+          {server.name}
+          <ChevronDown className="h-5 w-5 ml-auto" />
+        </button>
       </DropdownMenuTrigger>
     </DropdownMenu>
   );
